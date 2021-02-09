508f491930e2e42802fef06819852ff09de53103
AnnaGardt <60440521+AnnaGardt@users.noreply.github.com>
2021-02-09 14:11:11 +0700

    added offer section (with swiper slider)

--- js/script.js
+++ js/script.js
@@ -13,4 +13,30 @@ $(function(){
     $($(this).val()).addClass('tabs__content_active');
   });
   $('.tabs__mobile').styler();
+
+  const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    autoplay: true,
+    disableOnInteraction: false,
+    loop: true,
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+        spaceBetween: 10
+      },
+      600: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      },
+      767: {
+        slidesPerView: 4,
+        spaceBetween: 30
+      }
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+  });
 });
